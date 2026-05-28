4f7a4beca408410bd850a3f181d7e163000d7b99
zzzdajb <122362831+zzzdajb@users.noreply.github.com>
2026-05-28 22:19:57 +0800

    fix(mobile): config plugin for cleartext HTTP
    Use withAndroidManifest to inject android:usesCleartextTraffic
    into AndroidManifest.xml. Expo ignores non-standard app.json properties.

---
+++ apps/mobile/plugins/withCleartextTraffic.js
@@ -0,0 +1,13 @@
+const { withAndroidManifest, AndroidConfig } = require("expo/config-plugins");
+
+function withCleartextTraffic(config) {
+    return withAndroidManifest(config, (mod) => {
+        const mainApplication = AndroidConfig.Manifest.getMainApplication(mod.modResults);
+        if (mainApplication?.$) {
+            mainApplication.$["android:usesCleartextTraffic"] = "true";
+        }
+        return mod;
+    });
+}
+
+module.exports = withCleartextTraffic;
